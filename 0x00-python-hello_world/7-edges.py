ca63985e3a774f51641d16f88715ea2ecd9ad227
moharafat <mohamedamr0001@gmail.com>
2024-01-01 17:14:13 +0200

    127

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"First 3 letters: {word[0:2]}")
+print(f"Last 2 letters: {word[7:9]}")
+print(f"Middle word: {word[1:8]}")
+print('\n')
+print('\n')
