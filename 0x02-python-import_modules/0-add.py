812d02178cf0ac117d73bd455c771341f91106e8
moharafat <mohamedamr0001@gmail.com>
2024-01-04 18:55:06 +0200

    s

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-from add_0.py import def add(a, b):
-    a = 1
-    b = 2
+from add_0 import add
 
+a = 1
+b = 2
+sum = add(a, b)
+#only works if we are executing from main file
 if __name__ == "__main__":
-    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    print("{0} + {1} = {2}".format(a, b, sum))
