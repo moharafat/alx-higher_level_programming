fe15b4dd72846c84fb25fa62e4ba83b08a88810b
moharafat <mohamedamr0001@gmail.com>
2024-01-04 21:08:22 +0200

    s

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if argv == 0:
         print("0 arguments.")
     elif argv == 1:
-        print("1 argument")  
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
         print(argv, "arguments:")
