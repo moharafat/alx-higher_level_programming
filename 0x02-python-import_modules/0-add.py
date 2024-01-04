3494de7473766f200505edb14fb2b60d9f87e7bb
moharafat <mohamedamr0001@gmail.com>
2024-01-04 18:30:46 +0200

    s

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
-   
-   
+from add_0.py import def add(a, b):
+    a = 1
+    b = 2
+
 if __name__ == "__main__":
     print("{0} + {1} = {2}".format(a, b, add(a, b)))
