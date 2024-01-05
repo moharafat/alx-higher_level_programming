5b1c085b7881230cac4c32d0753f84e6c15439b5
moharafat <mohamedamr0001@gmail.com>
2024-01-05 17:30:39 +0200

    S

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+def no_c(my_string):
+    my_array = my_string.split()
+    for i in my_array:
+        if my_array[i] == 'c' or my_array[i] == 'C':
+            del my_string[i]
