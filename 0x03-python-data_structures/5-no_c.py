fa66d5da11215258a3e7e4e7fb1cb4606e7ec521
moharafat <mohamedamr0001@gmail.com>
2024-01-06 15:22:41 +0200

    s

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    my_array = my_string.split()
-    for char in my_array:
-        if my_array[char] == 'c' or my_array[char] == 'C':
+    my_string.split()
+    for char in my_string:
+        if my_string[char] == 'c' or my_string[char] == 'C':
             my_string.remove[char]
