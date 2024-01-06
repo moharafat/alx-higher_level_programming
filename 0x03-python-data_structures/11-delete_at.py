197209ed23e2dcb6be932c8770122b4150f1e984
moharafat <mohamedamr0001@gmail.com>
2024-01-06 23:00:06 +0200

    ب

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new_list = []
+    if idx < 0:
+        return my_list
     for idx in my_list:
         if idx == my_list[idx]:
-            new_list.append(my_list[idx + 1])
+
     return new_list
