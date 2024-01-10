a88aa2dcdf7e79df0008b01bd9692392b62fe85e
moharafat <mohamedamr0001@gmail.com>
2024-01-11 01:33:59 +0200

    s

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    multi = list(filter(lambda x: x * number, my_list))
+    multi = list(map(lambda x: x * number, my_list))
     return multi
