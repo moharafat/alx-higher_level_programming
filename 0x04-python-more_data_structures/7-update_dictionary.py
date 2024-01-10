0ad08be11ffc04127f9c2b2034f8f0606b33accf
moharafat <mohamedamr0001@gmail.com>
2024-01-10 22:43:08 +0200

    s

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-        sorted_keys = sorted(a_dictionary)
+    sorted_keys = sorted(a_dictionary)
+    for key in sorted_keys:
+        value = a_dictionary[key]
+        print(f"{key}: {value}")
