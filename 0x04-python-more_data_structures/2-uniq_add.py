5e0fa3807e3d2fe6d9c3b066197130093c279c95
moharafat <mohamedamr0001@gmail.com>
2024-01-10 20:56:16 +0200

    s

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique_values = set()
+    if len(my_list) == 0:
+        return 0
     for number in (my_list):
         if number not in unique_values:
             unique_values.add(number)
