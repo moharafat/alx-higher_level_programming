c01d6cf489814e34a5ce392eb073d3df878aeb89
moharafat <mohamedamr0001@gmail.com>
2024-01-24 01:09:05 +0200

    s

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" defining a Square"""
+
+
+class Square:
+    """empty class that is defining a square """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print()
