954e5ba5992368b3e72ae85ab896d3312df689ed
moharafat <mohamedamr0001@gmail.com>
2024-01-27 11:55:34 +0200

    n

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+contains add_integer function
+adds 2 integers or 1 integer to 98
+"""
 def add_integer(a, b=98):
+    """
+    adds 2 integers or 1 integer to 98
+    """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
