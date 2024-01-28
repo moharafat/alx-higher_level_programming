3050a4d4d4cb75deeee9a2755cc2b5fa74a82b5f
moharafat <mohamedamr0001@gmail.com>
2024-01-28 14:59:40 +0200

    s

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,11 +14,10 @@ def matrix_divided(matrix, div):
         for j in range(len(matrix[i])):    
             if not isinstance(matrix[i][j], (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            if len(matrix[i]) != len(matrix[i+1])
+            if len(matrix[i]) != len(matrix[i+1]):
                 raise TypeError("Each row of the matrix must have the same size")
             if not isinstance(div,(int, float)):
                 raise TypeError("div must be a number")
             if div == 0:
                 raise ZeroDivisionError("division by zero")
-            for i in matrix:
-                return matrix[i] / div
+            return matrix[i] / div
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,3 +5,38 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """Def.Unittest for max_integer([..]"""
+
+    def test_ordered(self):
+        """list made out of int [orderd]"""
+        ordered = [1, 2, 3, 4]
+        self.assertEqual(max_integer(ordered), 4)
+    
+    def test_max_begining(self):
+        max_at_begining = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_begining), 4)
+
+    def test_unordered(self):
+        """LIST MADE OUT OF INT BUT [UNORDERED]"""
+        unordered = [2,4,3,1]
+        self.assertEqual(max_integer(unordered), 4)
+
+    def test_only_1_elment(self):
+        only_1 = [9]
+        self.assertEqual(max_integer(only_1), 9)
+
+    def test_empty_list(self):
+        empty_list = []
+        self.assertEqual(max_integer(empty_list), None)
+
+    def test_float_list(self):
+        floaty = [33.4, 55.2, 44.9, 11.7]
+        self.assertEqual(max_integer(floaty), 55.2)
+    
+    def test_floats_and_integers(self):
+        floaty_and_inty = [3, 77.3, 11.3, 99]
+        self.assertEqual(max_integer(floaty_and_inty), 99)
+ 
+
+if __name__ == '__main__':
+    unittest.main()
