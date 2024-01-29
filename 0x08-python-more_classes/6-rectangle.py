3376498d059fa4aeb10f7497e14db45ee3373576
moharafat <mohamedamr0001@gmail.com>
2024-01-30 00:11:27 +0200

    s

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,10 +4,11 @@
 
 class Rectangle:
     """a rectangle"""
+
     counter = 0
 
     def __init__(self, width=0, height=0):
-        type(self).counter += 1
+        Rectangle.counter += 1
         self.width = width
         self.height = height
 
@@ -54,5 +55,6 @@ class Rectangle:
             )"
 
     def __del__(self):
-        type(self).counter -= 1
+        Rectangle.counter -= 1
         print("Bye rectangle...")
+
