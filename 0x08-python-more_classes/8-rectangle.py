6d12811d966b0f63fdd1a17e8cf03ba83019bebe
moharafat <mohamedamr0001@gmail.com>
2024-01-30 21:45:16 +0200

    s

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -11,9 +11,10 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
+
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
-        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -47,10 +48,21 @@ class Rectangle:
             return 0
         return (self.width * 2) + (self.height * 2)
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area >= rect_2.area:
+            return rect_1
+        else:
+            return rect_2
+        
     def __str__(self):
         if self.__height == 0 or self.__width == 0:
             return ""
-        return "\n".join([self.print_symbol * self.__width for row in range(self.__height)])
+        return "\n".join([str(self.print_symbol) * self.__width for row in range(self.__height)])
 
     def __repr__(self):
         xyz = ", "
@@ -58,5 +70,5 @@ class Rectangle:
             )"
 
     def __del__(self):
-        print("Bye rectangle...")
         type(self).number_of_instances -= 1
+        print("Bye rectangle...")
