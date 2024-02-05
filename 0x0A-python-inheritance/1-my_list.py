f8e0b0474c3f34d80217901ba2811fb9cb8ef3fe
moharafat <mohamedamr0001@gmail.com>
2024-02-05 18:32:05 +0200

    s

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+THIS FILE CONTAINS THE LOOKUP FUNC.
+THAT RETURNS THE LIST  OF AVALILABLE
+ATTRIBUTES&METHODS IN AN OBJ."""
+
+class MyList(list):
+    def print_sorted(self):
+        """Defining THE LOOKUP FUNC. WHICH RETURNS THE LIST  OF AVALILABLE
+        ATTRIBUTES&METHODS IN AN OBJ."""
+        return sorted(list)
