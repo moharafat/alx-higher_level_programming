074a1e4417ddd73649d54ee2627ee05fe006932a
moharafat <mohamedamr0001@gmail.com>
2024-02-05 18:41:01 +0200

    sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-THIS FILE CONTAINS THE LOOKUP FUNC.
-THAT RETURNS THE LIST  OF AVALILABLE
-ATTRIBUTES&METHODS IN AN OBJ."""
+Write a class MyList that inherits from list
+"""
+
 
 class MyList(list):
     def print_sorted(self):
-        """Defining THE LOOKUP FUNC. WHICH RETURNS THE LIST  OF AVALILABLE
-        ATTRIBUTES&METHODS IN AN OBJ."""
-        return sorted(list)
+        """that prints the list, but sorted (ascending sort)"""
+
+        print(sorted(self))
