38e12dee8ed92dfb8ea30c925a5af1fa8d9ecf4a
moharafat <mohamedamr0001@gmail.com>
2024-02-06 19:01:08 +0200

    d

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining a file-writing function."""
+import json
+
+
+def class_to_json(obj):
+    """
+    Returns:  JSON representation of an object (stringz).
+    """
+    return json.loads(my_str)
