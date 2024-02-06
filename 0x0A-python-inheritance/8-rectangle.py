4484549249a10c379af456dab9250d92d5477f60
moharafat <mohamedamr0001@gmail.com>
2024-02-06 09:17:52 +0200

    test

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """
- raises an Exception with the message area() is not implemented
+Defining a class Rctangle inherited from BaseGeometry
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """class BaseGeomtry"""
-    def area(self):
-        raise Exception("area() is not implemented")
-    def integer_validator(self, name, value):
-        if not isinstance (value, int):
-            raise TypeError("<name> must be an integer")
-        if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator(self, width)
+        self.__width = width
