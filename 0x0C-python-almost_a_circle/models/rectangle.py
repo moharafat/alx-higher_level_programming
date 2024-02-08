6d4a097af8dcedb12ffeca30f6da9d3e28b8f640
moharafat <mohamedamr0001@gmail.com>
2024-02-08 21:18:04 +0200

    x

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -33,6 +33,8 @@ class Rectangle(Base):
         return self.__x
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
@@ -41,6 +43,8 @@ class Rectangle(Base):
         return self.__y
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
