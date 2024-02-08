3196254056715a4b3450b4a0fb56f2a729ff9197
moharafat <mohamedamr0001@gmail.com>
2024-02-08 17:13:41 +0200

    readme

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+    
