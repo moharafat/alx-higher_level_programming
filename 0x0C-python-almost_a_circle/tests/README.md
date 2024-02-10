242aabbcacce570c1d9b68066b56778c481fdb61
moharafat <mohamedamr0001@gmail.com>
2024-02-10 14:51:03 +0200

    s

--- 0x0C-python-almost_a_circle/tests/README.md
+++
@@ -1,1 +0,0 @@
-hello high level programming
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defining unittest for the file base.py"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
