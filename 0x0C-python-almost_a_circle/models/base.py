44f64a773f48cc1de8d907fa43c385b157fcb7d2
moharafat <mohamedamr0001@gmail.com>
2024-02-12 22:21:03 +0200

    documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,10 +4,13 @@ import json
 
 
 class Base:
-    """Representing the base model """
+    """Representing the base model"""
+
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initilizing the base"""
         if id is not None:
             self.id = id
         else:
@@ -16,17 +19,21 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ Returs a JSON serliئation"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
+        """Returns the deserlization of a JSON  string"""
         if json_string is None or len(json_string) == 0:
             return "[]"
         return json.loads(json_string)
+
     @classmethod
     def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
         if (dictionary) and dictionary != {}:
             if cls.__name__ == "Rectangle":
                 new_class = cls(1,1)
