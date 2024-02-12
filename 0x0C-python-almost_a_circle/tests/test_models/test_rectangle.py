696a3ca1af905766f9d24f25ecd4a0dc8b3b5ec0
moharafat <mohamedamr0001@gmail.com>
2024-02-12 13:29:12 +0200

    s

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,10 +5,6 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
     @classmethod
-    def test_width_setter(self):
-        r = Rectangle(5, 7, 7, 5, 1)
-        r.width = 10
-        self.assertEqual(r.width, 10)
 
 if __name__ == '__main__':
     unittest.main()
