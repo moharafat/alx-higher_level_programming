ba44ecb4d81907223c972af346b12ec43996a8a1
moharafat <mohamedamr0001@gmail.com>
2024-02-12 19:31:01 +0200

    s

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,7 +82,7 @@ class Rectangle(Base):
 
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}"\
-    .format(self.id, self.__x, self.__y, self.__width, self.__height)
+    .format(self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
         if args:
