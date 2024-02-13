4911b6219e0587ecd29f38aae5dc4c4fe75067c4
moharafat <mohamedamr0001@gmail.com>
2024-02-13 21:33:39 +0200

    s

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,6 +2,10 @@
 """Defining unittest for the file rectangle.py"""
 import unittest
 from models.rectangle import Rectangle
+import io
+from contextlib import redirect_stdout
+
+
 
 class TestRectangle(unittest.TestCase):
     """Unittest for the rectangle func."""
@@ -69,6 +73,29 @@ class TestRectangle(unittest.TestCase):
     def test_area(self):
         My_rect = Rectangle(3, 2)
         self.assertEqual(My_rect.area(), 6)
+        
+    def test_display_no_x_and_y(self):
+        Myrec = Rectangle(4, 2)
+        The_expected = "####\n####\n"
+        with io.StringIO() as buf, redirect_stdout(buf):
+            Myrec.display()
+            The_Output =  buf.getvalue()
+            self.assertEqual(The_Output, The_expected)
 
+    def test_display_no_y(self):
+        Myrec = Rectangle(4, 2, 1)
+        The_expected = " ####\n ####\n"
+        with io.StringIO() as buf, redirect_stdout(buf):
+            Myrec.display()
+            The_Output =  buf.getvalue()
+            self.assertEqual(The_Output, The_expected)
+
+    def test_display_Full(self):
+        Myrec = Rectangle(4, 2, 1, 2)
+        The_expected = "\n\n ####\n ####\n"
+        with io.StringIO() as buf, redirect_stdout(buf):
+            Myrec.display()
+            The_Output =  buf.getvalue()
+            self.assertEqual(The_Output, The_expected)
 if __name__ == '__main__':
     unittest.main()
