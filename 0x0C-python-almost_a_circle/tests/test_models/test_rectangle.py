46d0d306a2dc17b0dd698b230dc6c5eb7220139d
moharafat <mohamedamr0001@gmail.com>
2024-02-13 16:46:43 +0200

    s

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -4,7 +4,21 @@ import unittest
 from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
-    @classmethod
+    """Unittest for the rectangle func."""
+    def test_2_args(self):
+        My_rect = Rectangle(77, 99)
+        self.assertEqual(My_rect.width, 77)
+        self.assertEqual(My_rect.height, 99)
+        self.assertEqual(My_rect.x, 0)
+        self.assertEqual(My_rect.y, 0)
+    
+    def test_4_args(self):
+        My_rect = Rectangle(77, 99, 8, 1)
+        self.assertEqual(My_rect.width, 77)
+        self.assertEqual(My_rect.height, 99)
+        self.assertEqual(My_rect.x, 8)
+        self.assertEqual(My_rect.y, 1)
+
 
 
 if __name__ == '__main__':
