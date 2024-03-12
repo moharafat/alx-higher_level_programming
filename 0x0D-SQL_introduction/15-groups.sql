d9f8fefa3b8a0b1a34c948c39a3646e178cb0b9f
Mohamed Amr <mohamedamr0001@.com>
2024-03-12 23:16:11 +0200

    GROUP BYYYY

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 --  lists the number of records with the same score in the table second_table
 
-SELECT score, count(*) AS numberFROM second_table ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
