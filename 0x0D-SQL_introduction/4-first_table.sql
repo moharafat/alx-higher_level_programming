cc68b2a02a603105fceb0493f45011264dc28d3d
Mohamed Amr <mohamedamr0001@.com>
2024-03-12 20:34:15 +0200

    NOT NULLL

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,6 @@
+-- creates a table called first_table in the current database
+
 CREATE TABLE first_table(
     id INT NOT NULL,
-    name VARCHAR(256) NOT NULL);
+    name VARCHAR(256) NOT NULL
+    );
