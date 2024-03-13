8b478cc8bbd9a31ebe488efc5778b111a7e5050f
Mohamed Amr <mohamedamr0001@.com>
2024-03-14 00:05:08 +0200

    LEFT INNER XXX

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,4 +4,4 @@ SELECT TV.title, G.genre_id
 FROM tv_shows AS TV
     LEFT JOIN tv_show_genres AS G
     ON TV.id = G.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY TV.title, G.genre_id;
