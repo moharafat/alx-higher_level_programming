28902dbb26e8b1904e460df8073803960e1a2d90
Mohamed Amr <mohamedamr0001@.com>
2024-03-14 00:56:12 +0200

    S

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,5 @@ SELECT G.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres AS G
     INNER JOIN tv_show_genres AS TV
     ON G.id = TV.genre_id
+    GROUP BY G.name
 ORDER BY number_of_shows DESC;
