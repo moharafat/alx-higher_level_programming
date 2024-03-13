e8747285ddc321b15bbd06fe420bd81b804208c5
Mohamed Amr <mohamedamr0001@.com>
2024-03-14 01:49:00 +0200

    h

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,12 @@
+-- Import the database dump from hbtn_0d_tvshows
+
+SELECT G.name
+FROM tv_genres AS G
+    INNER JOIN tv_show_genres AS S
+    ON G.id = S.genre_id
+
+    INNER JOIN tv_shows AS TV
+    ON S.show_id = TV.id
+    WHERE TV.title = "Dexter"
+
+ORDER BY G.name;
