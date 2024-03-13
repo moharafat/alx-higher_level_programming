e033b84d3d6e0d2d9a762c23da91b2352cf82e1c
Mohamed Amr <mohamedamr0001@.com>
2024-03-14 00:46:43 +0200

    grouped

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+
+SELECT G.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres AS G
+    INNER JOIN tv_show_genres AS TV
+    ON G.id = TV.genre_id
+ORDER BY number_of_shows DESC;
