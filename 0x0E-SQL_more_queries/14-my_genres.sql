18480f197660e3d6ec03e468e239d5bab3974b97
Mohamed Amr <mohamedamr0001@.com>
2024-03-14 02:00:40 +0200

    S

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,9 +4,9 @@ SELECT G.name
 FROM tv_genres AS G
     INNER JOIN tv_shows AS TV
     ON S.show_id = TV.id
-    WHERE TV.title = "Dexter"
     INNER JOIN tv_show_genres AS S
     ON G.id = S.genre_id
 
+    WHERE TV.title = "Dexter"
 
 ORDER BY G.name;
