0dfa0c25d2ff42a9be3e278dee23c7fd527c6fdb
Mohamed Amr <mohamedamr0001@.com>
2024-04-08 22:26:00 +0200

    slice

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,8 @@
 
 const argv = process.argv.slice(2);
 
-
 if (argv.length < 2) {
   console.log('0');
 } else {
-  console.log(argv.sort((a, b) => b - a)[1])
+  console.log(argv.sort((a, b) => b - a)[1]);
 }
