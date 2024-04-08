1916b017e37c61d5fc381d78530fd5a8535f4144
Mohamed Amr <mohamedamr0001@.com>
2024-04-08 17:54:11 +0200

    d

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let count = 0;
 while (count <= 2) {
-  console.log(myvar[count]);
+  console.log(myVar[count]);
   count++;
 }
