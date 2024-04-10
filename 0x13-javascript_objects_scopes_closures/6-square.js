f0d84ebfe382edb87bdb5feee3b88c5df29849f7
Mohamed Amr <mohamedamr0001@.com>
2024-04-10 02:57:33 +0200

    charPrint (c = 'X') {

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const Square_old = require('./5-square');
-class Square extends Square_old {
+const SquareOld = require('./5-square');
+class Square extends SquareOld {
   constructor (size) {
     super(size, size);
   }
-  charPrint(c = 'X') {
+
+  charPrint (c = 'X') {
     if (c === undefined);
     for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
   }
