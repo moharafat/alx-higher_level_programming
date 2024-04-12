6c3ffd65d0c073d545b4f3827c6d95cdc1271c66
Mohamed Amr <mohamedamr0001@.com>
2024-04-13 00:26:29 +0200

    s

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""lists all states from database"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost",
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
