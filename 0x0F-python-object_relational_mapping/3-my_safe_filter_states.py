ee851867ccc1ef4face9e8a2222a96cd1ea99152
Mohamed Amr <mohamedamr0001@.com>
2024-04-13 03:51:00 +0200

    index

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,17 +16,9 @@ if __name__ == '__main__':
                            db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
-    try:
-        query = ("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
-        cur.execute(query, (sys.argv[4],))
-        query_rows = cur.fetchall()
-
-    except MySQLdb.Error as e:
-        try:
-            print(f"MySQL Error [{e.args[0]}]: {e.args[1]}")
-        except IndexError:
-            print(f"MySQL Error: {str(e)}")
-        
+    query = ("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
+    cur.execute(query, (sys.argv[4],))
+    query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
