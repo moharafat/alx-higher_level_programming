128a67814a602480f001f5bb9ca9700c9eebdf74
Mohamed Amr <mohamedamr0001@.com>
2024-04-13 03:36:48 +0200

    cur.execute(query, (sys.argv[4],))

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""lists all states from database"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print("{}".format(sys.argv[0]))
+        sys.exit(1)
+
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    query = ("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
+    cur.execute(query, (sys.argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
