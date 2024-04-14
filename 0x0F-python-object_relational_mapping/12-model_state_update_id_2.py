c1e62a5d29739beeed2d2dd27976ab9f09e2587d
Mohamed Amr <mohamedamr0001@.com>
2024-04-14 22:22:34 +0200

    d

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists all states from database hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    My_Session = Session()
+
+    mexico_update = My_Session.query(State).filter(State.id == 2).first()
+
+    mexico_update.name = "Mexico"
+    My_Session.commit()
