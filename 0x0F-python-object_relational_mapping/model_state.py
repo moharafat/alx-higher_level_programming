ee64466fb9ac9f0bb52a597cf54cbcfdd6405631
Mohamed Amr <mohamedamr0001@.com>
2024-04-14 02:59:41 +0200

    state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""This module creates a model state
-"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,12 +6,9 @@ Base = declarative_base()
 
 
 class State(Base):
-    """this class creates a new mode State
-
-    Args:
-        Base (base): from sqlalchemy
+    """Represents a state for my sql table
+    which creates a new model stae
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    
