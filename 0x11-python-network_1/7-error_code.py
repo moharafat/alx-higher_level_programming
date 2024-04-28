2a3f515a5f38d8d6c4c067500d4cfc9a139ee050
Mohamed Amr <mohamedamr0001@.com>
2024-04-29 01:41:37 +0300

    .text

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" takes in a URL and an email, sends POST request to the passed URL with the
+email as a parameter, and finally displays the body of the response
+"""
+import sys
+import requests
+if __name__ == "__main__":
+    URL = sys.argv[1]
+   
+    response = requests.get(URL)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
+
