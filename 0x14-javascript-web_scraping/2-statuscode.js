410c45747b314a8511138b52edfaf711447ade64
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-05-21 22:11:05 +0300

    response

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], (error, response, body) => {
+request(process.argv[2], function (error, response, body) {
   if (error) throw error;
-  console.log('code:', response && response.statusCode);
+  console.log(`code: ${response.statusCode}`);
 });
