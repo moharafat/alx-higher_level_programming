88ae756670c637b5b778071aa70afaabd269bc1f
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-05-21 22:29:16 +0300

    lex X const

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-URL = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2]
+const URL = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(URL, function (error, response, body) {
   if (error) throw error;
-  console.log(body);
+  console.log(JSON.parse(body).title);
 });
